2bd68b2250e9579dea7e6f8221ed6effdcd528ef
Vince Linise <contact@vincelinise.com>
2025-12-26 15:27:33 +0100

    fix: BarTitle lint issues

--- src/components/BarTitle/BarTitle.jsx
+++ src/components/BarTitle/BarTitle.jsx
@@ -4,7 +4,8 @@ import { useI18n } from 'twake-i18n'
 import { BarLeft } from 'cozy-bar'
 import ButtonCozyHome from 'cozy-bar/dist/components/utils/ButtonCozyHome'
 import Divider from 'cozy-ui/transpiled/react/Divider'
-import Typography from "cozy-ui/transpiled/react/Typography"
+import Typography from 'cozy-ui/transpiled/react/Typography'
+
 import LearningAssistantIcon from '@/assets/icons/LearningAssistantIcon'
 
 const BarTitle = () => {
@@ -12,10 +13,10 @@ const BarTitle = () => {
 
   return (
     <BarLeft>
-      <div className='u-flex u-flex-items-center'>
+      <div className="u-flex u-flex-items-center">
         <ButtonCozyHome />
         <Divider orientation="vertical" flexItem />
-        <div className='u-flex u-flex-items-center u-ml-1'>
+        <div className="u-flex u-flex-items-center u-ml-1">
           <LearningAssistantIcon />
           <Typography variant="h4" className="u-ml-half">
             {t('title')}
