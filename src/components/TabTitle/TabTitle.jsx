7ef5a4978063630d593e2177788388517c557fb1
Vince Linise <contact@vincelinise.com>
2025-12-29 11:22:10 +0100

    feat: Add back button functionality

--- src/components/TabTitle/TabTitle.jsx
+++ src/components/TabTitle/TabTitle.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const TabTitle = ({ children, trailing }) => {
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import IconButton from 'cozy-ui/transpiled/react/IconButton'
+import PreviousIcon from 'cozy-ui/transpiled/react/Icons/Previous'
+
+const TabTitle = ({ children, trailing, backEnabled = false }) => {
+  const navigate = useNavigate()
+
+  const handleBack = () => {
+    navigate(-1)
+  }
+
   return (
     <div className="u-flex u-flex-items-start u-p-2">
-      <div className="u-flex u-flex-column u-flex-justify-center u-flex-grow-1">{children}</div>
+      <div className="u-flex u-flex-row u-flex-justify-center u-flex-grow-1">
+        {backEnabled && (
+          <IconButton
+            className="u-p-0 u-mr-1 u-w-2 u-h-2"
+            style={{ marginTop: -2 }}
+            onClick={handleBack}
+          >
+            <Icon icon={PreviousIcon} />
+          </IconButton>
+        )}
+        <div className="u-flex u-flex-column u-flex-justify-center u-flex-grow-1">
+          {children}
+        </div>
+      </div>
       <div className="u-flex u-flex-items-center">{trailing}</div>
     </div>
   )
--- src/views/InsightsTab.jsx
+++ src/views/InsightsTab.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { useI18n } from 'twake-i18n'
 
-import Button from 'cozy-ui/transpiled/react/Buttons'
-import Icon from 'cozy-ui/transpiled/react/Icon'
-import PlusIcon from 'cozy-ui/transpiled/react/Icons/Plus'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 
 import FilterChip from '@/components/FilterChip/FilterChip'
