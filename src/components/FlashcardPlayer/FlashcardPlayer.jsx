6214830025a8e902fedfd8cabfc2478ed4f44347
Vince Linise <contact@vincelinise.com>
2026-02-03 11:38:30 +0100

    feat(Flashcards): Add 3D preview

--- src/components/FlashcardPlayer/FlashcardPlayer.jsx
+++ src/components/FlashcardPlayer/FlashcardPlayer.jsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames'
+import { motion, AnimatePresence } from 'motion/react'
 import React from 'react'
 
 import Typography from 'cozy-ui/transpiled/react/Typography'
@@ -10,26 +11,33 @@ export default function FlashcardPlayer({ flashcard }) {
   const [showAnswer, setShowAnswer] = React.useState(false)
 
   return (
-    <div
-      className={classNames(
-        'u-flex u-flex-column u-flex-items-center u-flex-justify-center',
-        styles.flashcard
-      )}
-      onClick={() => setShowAnswer(!showAnswer)}
-    >
-      {!showAnswer ? (
-        <>
-          <Typography variant="h3" align="center">
-            {question}
-          </Typography>
-        </>
-      ) : (
-        <>
-          <Typography variant="h3" align="center">
-            {answer}
-          </Typography>
-        </>
-      )}
-    </div>
+    <AnimatePresence>
+      <motion.div
+        className={classNames(
+          'u-flex u-flex-column u-flex-items-center u-flex-justify-center',
+          styles.flashcard
+        )}
+        onClick={() => setShowAnswer(!showAnswer)}
+        initial={{ rotateY: -180, scale: 0.8 }}
+        animate={{ rotateY: 0, scale: 1 }}
+        exit={{ rotateY: 180, scale: 0.8 }}
+        transition={{ duration: 1.3, type: 'spring', bounce: 0.3 }}
+        key={flashcard._id + (showAnswer ? ':answer' : ':question')}
+      >
+        {!showAnswer ? (
+          <>
+            <Typography variant="h3" align="center">
+              {question}
+            </Typography>
+          </>
+        ) : (
+          <>
+            <Typography variant="h3" align="center">
+              {answer}
+            </Typography>
+          </>
+        )}
+      </motion.div>
+    </AnimatePresence>
   )
 }
