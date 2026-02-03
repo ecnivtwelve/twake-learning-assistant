4dae222dbfc31782406f025318abe5be528b45ff
Vince Linise <contact@vincelinise.com>
2026-02-03 12:20:33 +0100

    feat(rag): Customize number of generated flashcards

--- src/views/Item/ItemView.jsx
+++ src/views/Item/ItemView.jsx
@@ -11,6 +11,15 @@ import { makeActions } from 'cozy-ui/transpiled/react/ActionsMenu/Actions'
 import Button from 'cozy-ui/transpiled/react/Buttons'
 import Checkbox from 'cozy-ui/transpiled/react/Checkbox'
 import CircularProgress from 'cozy-ui/transpiled/react/CircularProgress'
+import {
+  DialogBackButton,
+  DialogCloseButton,
+  useCozyDialog
+} from 'cozy-ui/transpiled/react/CozyDialogs'
+import Dialog, {
+  DialogTitle,
+  DialogActions
+} from 'cozy-ui/transpiled/react/Dialog'
 import Divider from 'cozy-ui/transpiled/react/Divider'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import NewIcon from 'cozy-ui/transpiled/react/Icons/New'
@@ -19,6 +28,8 @@ import TrashIcon from 'cozy-ui/transpiled/react/Icons/Trash'
 import List from 'cozy-ui/transpiled/react/List'
 import ListItem from 'cozy-ui/transpiled/react/ListItem'
 import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
+import MenuItem from 'cozy-ui/transpiled/react/MenuItem'
+import TextField from 'cozy-ui/transpiled/react/TextField'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import { useAlert } from 'cozy-ui/transpiled/react/providers/Alert'
 
@@ -118,6 +129,29 @@ const ItemView = () => {
     }
   }
 
+  const [customizeGenerationDialog, setCustomizeGenerationDialog] =
+    useState(false)
+
+  const [numberOfQuestions, setNumberOfQuestions] = useState(5)
+  const numberOfQuestionsOptions = [
+    {
+      value: 5,
+      label: '5 questions'
+    },
+    {
+      value: 10,
+      label: '10 questions'
+    },
+    {
+      value: 15,
+      label: '15 questions'
+    },
+    {
+      value: 20,
+      label: '20 questions'
+    }
+  ]
+
   const [isGenerating, setIsGenerating] = useState(false)
 
   const ragGenerate = async () => {
@@ -128,7 +162,7 @@ const ItemView = () => {
         'Numérique et sciences informatiques',
         16,
         activity.title,
-        5,
+        numberOfQuestions,
         false
       )
       const json = extractJSONObject(data.choices[0].message.content)
@@ -144,6 +178,11 @@ const ItemView = () => {
             }
           })
         )
+
+        showAlert({
+          message: t('questions.generate.success'),
+          severity: 'success'
+        })
       } else {
         log.error(json)
         showAlert({
@@ -161,6 +200,19 @@ const ItemView = () => {
     setIsGenerating(false)
   }
 
+  const {
+    dialogProps,
+    dialogTitleProps,
+    listItemProps,
+    dividerProps,
+    dialogActionsProps
+  } = useCozyDialog({
+    size: 'medium',
+    open: customizeGenerationDialog,
+    onClose: () => setCustomizeGenerationDialog(false),
+    disableEnforceFocus: true
+  })
+
   return (
     <div className="u-flex u-flex-column u-h-100">
       <TabTitle
@@ -171,7 +223,7 @@ const ItemView = () => {
               variant="secondary"
               label={t('generate')}
               startIcon={<Icon icon={NewIcon} />}
-              onClick={() => ragGenerate()}
+              onClick={() => setCustomizeGenerationDialog(true)}
               className="u-mr-1"
             />
 
@@ -234,6 +286,48 @@ const ItemView = () => {
           ))}
         </div>
       </TabTitle>
+
+      <Dialog {...dialogProps}>
+        <DialogCloseButton
+          onClick={() => setCustomizeGenerationDialog(false)}
+        />
+        <DialogTitle {...dialogTitleProps}>Générer des questions</DialogTitle>
+        <Divider {...dividerProps} />
+        <div className="u-m-1">
+          <TextField
+            select
+            options={numberOfQuestionsOptions}
+            value={numberOfQuestions}
+            onChange={e => setNumberOfQuestions(e.target.value)}
+            label="Nombre de questions"
+            variant="outlined"
+            fullWidth
+          >
+            {numberOfQuestionsOptions.map(option => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </div>
+        <Divider {...dividerProps} />
+        <DialogActions {...dialogActionsProps}>
+          <Button
+            variant="secondary"
+            label={t('cancel')}
+            onClick={() => setCustomizeGenerationDialog(false)}
+          />
+          <Button
+            variant="primary"
+            label={t('generate')}
+            startIcon={<Icon icon={NewIcon} />}
+            onClick={() => {
+              setCustomizeGenerationDialog(false)
+              ragGenerate()
+            }}
+          />
+        </DialogActions>
+      </Dialog>
 
       {selectedQuestions.length > 0 && (
         <ActionsBar
