2ad39febbd3cbf8c8b159db1eb42ae2912aff3a9
Vince Linise <contact@vincelinise.com>
2026-02-23 09:50:34 +0100

    fix: Use flags for consts

--- src/consts/consts.js
+++ src/consts/consts.js
@@ -1,9 +1,6 @@
 import flag from 'cozy-flags'
 
-import env from './env.json'
-
-export const OPENRAG_URL = env.OPENRAG_URL || 'learnings.openrag.url'
-export const AUTH_TOKEN = env.AUTH_TOKEN || flag('learnings.openrag.token')
-
-export const LUCIE_URL = env.LUCIE_URL || flag('learnings.lucie.url')
-export const LUCIE_API_KEY = env.LUCIE_API_KEY || flag('learnings.lucie.token')
+export const OPENRAG_URL = flag('learnings.openrag.url')
+export const AUTH_TOKEN = flag('learnings.openrag.token')
+export const LUCIE_URL = flag('learnings.lucie.url')
+export const LUCIE_API_KEY = flag('learnings.lucie.token')
